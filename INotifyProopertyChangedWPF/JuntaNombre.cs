246e2346a60fdd5108e04d48d00d75f1b16960f9
HectorOC-e <osoriohector89@gmail.com>
2023-01-04 12:13:27 -0600

    Agregar archivos de proyecto.

---
+++ INotifyProopertyChangedWPF/JuntaNombre.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INotifyProopertyChangedWPF
+{
+    public class JuntaNombre : INotifyPropertyChanged
+    {
+        private string nombre, apellido, nombreCompleto;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Nombre
+        {
+            get => nombre;
+            set {
+                nombre = value;
+                OnPropertyChanged(nameof(nombreCompleto));
+                    }
+        }
+        public string Apellido
+        {
+            get => apellido;
+            set { 
+                apellido = value;
+                OnPropertyChanged(nameof(nombreCompleto));
+            }
+        }
+
+        public string NombreCompleto
+        {
+            get => Nombre + Apellido;
+            set { }
+        }
+    }
+}
